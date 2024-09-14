0d2edb574fb8cf30a0ea55d95b06f3b239cbb3b9
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2024-09-14 18:51:49 +0300

    completed

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 const rows = document.getElementById("rows");
 const cols = document.getElementById("columns");
 const btn = document.getElementById("submit");
-// const gridCtn = document.getElementById("container");
 const footer = document.getElementById("footer");
 const date = new Date();
+let drawing = false;
 
-footer.innerHTML = `<span> The Odin Project &copy;${date.getFullYear()} Steve Okoth`;
+footer.innerHTML = `<span> The Odin Project &copy;${date.getFullYear()} Steve Okoth</span>`;
 
-btn.addEventListener('click', function() {
-    // Get the number of rows and columns
-    const row = parseInt(rows.value);
-    const col = parseInt(cols.value);
+btn.addEventListener("click", function () {
+  const row = parseInt(rows.value);
+  const col = parseInt(cols.value);
+  const container = document.getElementById("container");
 
-    // Get the container element
-    const container = document.getElementById('container');
+  container.innerHTML = "";
+  container.style.display = "grid";
+  container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
 
-
-    // Set the CSS grid template based on the number of columns
-    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
-
-    // Create the grid elements
-    for (let i = 0; i < row * col; i++) {
-        const gridEl = document.createElement('div');
-        gridEl.className = 'gridEl';
-        container.appendChild(gridEl);
-    }
+  for (let i = 0; i < row * col; i++) {
+    const gridEl = document.createElement("div");
+    gridEl.className = "gridEl";
+    container.appendChild(gridEl);
+  }
 });
+
+document.addEventListener("mousedown", function (event) {
+  if (event.button === 0) {
+    drawing = true;
+  }
+});
+
+document.addEventListener("mouseup", function (event) {
+
+  if (event.button === 0) {
+    drawing = false;
+  }
+});
+
+document.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+});
+
+document.addEventListener("mouseover", function (event) {
+  if (drawing && event.target.classList.contains("gridEl")) {
+    event.target.style.backgroundColor = "black";
+  }
+});
+
